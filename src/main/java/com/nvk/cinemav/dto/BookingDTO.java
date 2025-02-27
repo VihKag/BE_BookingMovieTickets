68afe2fc6a8e57c0b28f4d766817e80920f57edd
ViKa <nvkhang191l@gmail.com>
2025-02-28 04:29:49 +0700

    update

--- src/main/java/com/nvk/cinemav/dto/BookingDTO.java
+++ src/main/java/com/nvk/cinemav/dto/BookingDTO.java
@@ -2,17 +2,24 @@ package com.nvk.cinemav.dto;
 
 import com.nvk.cinemav.entity.Payment;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class TicketDTO {
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingDTO {
   private UUID userId;
   private UUID showId;
-  private Integer seatId;
-  private Float price;
+  private List<Integer> seatId;
+  private Integer price;
   private LocalDateTime bookingDate;
   private Payment payment;
+  private String orderInfo;
+  private String email;
 }
